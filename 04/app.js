cfba165763044f81d39d81bf1fdb1f3d4dca1ab4
MariaEduardaAuadLopes <maria.ealopes2@senacsp.edu.br>
2022-11-30 20:28:49 -0300

    30/11

---
+++ 04/app.js
@@ -0,0 +1,15 @@
+function Verificar(){
+    let periodo = document.getElementById("verificar").value 
+
+    if(periodo == "vogal"){
+        document.getElementById("resultado").innerHTML = "vogal"
+    }
+
+    else if(periodo == "consoante"){
+        document.getElementById("resultado").innerHTML = "consoante"
+
+    }
+
+
+    }
+    
